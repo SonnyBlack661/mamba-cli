df27e5b92a00dae138d1902093dd53be7380e985
Jaime Costa Marques <jaime.mqu@gmail.com>
2020-06-02 13:46:07 -0300

    fix: 🐛 cli ts build

--- src/commands/mamba.ts
+++ src/commands/mamba.ts
@@ -1,7 +1,7 @@
 import { GluegunCommand } from 'gluegun';
 
 const command: GluegunCommand = {
-  name: 'mamba',
+  description: '🐍 MAMBA CLI 🐍',
   run: async (toolbox) => {
     const { print } = toolbox;
 
